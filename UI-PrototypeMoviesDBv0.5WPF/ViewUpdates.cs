4a2a5c0211b070045712c51cf9ddc231b5dc6ed5
quyx13 <ostendorf@gmx.com>
2021-09-22 09:38:32 +0200

    z

--- UI-PrototypeMoviesDBv0.5WPF/ViewUpdates.cs
+++ UI-PrototypeMoviesDBv0.5WPF/ViewUpdates.cs
@@ -1,12 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 namespace UI_PrototypeMoviesDBv0._5WPF
 {
     public static class ViewUpdates
     {
+        public static void UpdateBtnStart(Dispatcher dispatcher,
+            Dictionary<string, Object> controls, bool isEnabled)
+        {
+            var btnStart = (Button)controls["btnStart"];
+
+            dispatcher.BeginInvoke(new Action(() =>
+            {
+                btnStart.IsEnabled = isEnabled;
+            }), DispatcherPriority.Background);
+        }
+
+        public static void UpdateBtnStartImg(Dispatcher dispatcher,
+            Dictionary<string, Object> controls, string text)
+        {
+            var btnStartImg = (Image)controls["btnStartImg"];
+
+            dispatcher.BeginInvoke(new Action(() =>
+            {
+                btnStartImg.Source = new BitmapImage(new Uri(text, UriKind.Relative));
+            }), DispatcherPriority.Background);
+        }
+
+        public static void UpdateBtnStartTxt(Dispatcher dispatcher,
+            Dictionary<string, Object> controls, string text)
+        {
+            var btnStartTxt = (TextBlock)controls["btnStartTxt"];
+
+            dispatcher.BeginInvoke(new Action(() =>
+            {
+                btnStartTxt.Text = text;
+            }), DispatcherPriority.Background);
+        }
+
+        public static void UpdateBtnSettings(Dispatcher dispatcher,
+            Dictionary<string, Object> controls, bool isEnabled)
+        {
+            var btnSettings = (Button)controls["btnSettings"];
+
+            dispatcher.BeginInvoke(new Action(() =>
+            {
+                btnSettings.IsEnabled = isEnabled;
+            }), DispatcherPriority.Background);
+        }
+
+        public static void UpdateBtnSettingsImg(Dispatcher dispatcher,
+            Dictionary<string, Object> controls, string text)
+        {
+            var btnSettingsImg = (Image)controls["btnSettingsImg"];
+
+            dispatcher.BeginInvoke(new Action(() =>
+            {
+                btnSettingsImg.Source = new BitmapImage(new Uri(text, UriKind.Relative));
+            }), DispatcherPriority.Background);
+        }
+
+        public static void UpdateBtnSettingsTxt(Dispatcher dispatcher,
+            Dictionary<string, Object> controls, string text)
+        {
+            var btnSettingsTxt = (TextBlock)controls["btnSettingsTxt"];
+
+            dispatcher.BeginInvoke(new Action(() =>
+            {
+                btnSettingsTxt.Text = text;
+            }), DispatcherPriority.Background);
+        }
+
         public static void UpdateComboBox(Dispatcher dispatcher,
             Dictionary<string, Object> controls, string text)
         {
--- UI-PrototypeMoviesDBv0.5WPF/Worker.cs
+++ UI-PrototypeMoviesDBv0.5WPF/Worker.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
-using System.Windows.Controls;
 using System.Windows.Threading;
 
 namespace UI_PrototypeMoviesDBv0._5WPF
@@ -12,13 +11,6 @@ namespace UI_PrototypeMoviesDBv0._5WPF
         public static void DoWork(Dispatcher dispatcher, Dictionary<string, Object> controls, int number)
         {
             Trace.WriteLine("started...");
-
-            var btnStart = (Button)controls["btnStart"];
-            var btnStartImg = (Image)controls["btnStartImg"];
-            var btnStartTxt = (TextBlock)controls["btnStartTxt"];
-            var btnSettings = (Button)controls["btnSettings"];
-            var btnSettingsImg = (Image)controls["btnSettingsImg"];
-            var btnSettingsTxt = (TextBlock)controls["btnSettingsTxt"];
 
             ViewUpdates.SetupStatusProgressBar(dispatcher, controls, 0, number);
             
