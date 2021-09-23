9841c68c646221d77a423650e42a42b9869f47ef
quyx13 <ostendorf@gmx.com>
2021-09-23 05:11:18 +0200

    z

--- UI-PrototypeMoviesDBv0.5WPF/Worker.cs
+++ UI-PrototypeMoviesDBv0.5WPF/Worker.cs
@@ -31,27 +31,36 @@ namespace UI_PrototypeMoviesDBv0._5WPF
             state = State.running;
             ViewUpdates.SetStateRunning(dispatcher, controls, number);
 
-            ViewUpdates.UpdateStatusTextTime(dispatcher, controls, "JO");
-
             for (int i = 0; i < number; )
             {
                 #region Actually Working Area
-                
+
                 if (i > 0)
                 {
-                    var timeLeft = TimeSpan.FromMilliseconds((number - i) * ((int)timer.Elapsed.TotalMilliseconds / i));
+
                     //ViewUpdates.UpdateStatusTextTime(dispatcher, controls, 
                     //    $"{timer.Elapsed.Hours:D2}h:{timer.Elapsed.Minutes:D2}m:{timer.Elapsed.Seconds:D2}s (remaining: {timeLeft.Hours:D2}h:{timeLeft.Minutes:D2}m:{timeLeft.Seconds:D2}s)");
-                    Trace.WriteLine($"{timer.Elapsed.Hours:D2}h:{timer.Elapsed.Minutes:D2}m:{timer.Elapsed.Seconds:D2}s (remaining: {timeLeft.Hours:D2}h:{timeLeft.Minutes:D2}m:{timeLeft.Seconds:D2}s)");
-                    ViewUpdates.UpdateStatusTextTime(dispatcher, controls, i.ToString());
+                    //Trace.WriteLine($"{timer.Elapsed.Hours:D2}h:{timer.Elapsed.Minutes:D2}m:{timer.Elapsed.Seconds:D2}s (remaining: {timeLeft.Hours:D2}h:{timeLeft.Minutes:D2}m:{timeLeft.Seconds:D2}s)");
+
+                    var timeLeft = TimeSpan.FromMilliseconds((number - i) * ((int)timer.Elapsed.TotalMilliseconds / i));
+
+                    string text = $"number:{number}\t";
+                    text += $"i:{i}\t";
+                    text += $"elapsed:{(int)timer.Elapsed.TotalMilliseconds}\t";
+                    text += $"remaining:{(int)timeLeft.TotalMilliseconds}\t";
+
+                    ViewUpdates.UpdateStatusTextTime(dispatcher, controls, text);
+
+                    ViewUpdates.UpdateTextBox(dispatcher, controls, text);
                 }
-                
+
+                ViewUpdates.UpdateStatusTextTime(dispatcher, controls, i.ToString());
                 ViewUpdates.UpdateStatusTextTask(dispatcher, controls, 
                     $"{string.Format("{0:0,0}", (i + 1))} of {string.Format("{0:0,0}", (number))}");
                 ViewUpdates.UpdateStatusProgressBar(dispatcher, controls);
                 ViewUpdates.UpdateStatusTextPercentage(dispatcher, controls, 
                     $"{((i + 1) / numberDouble * 100):F2}%");
-                
+
                 i++;
                 #endregion
             }
