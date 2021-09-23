68a61c7c9035e2868fb8493debe17b1892e5edb4
quyx13 <ostendorf@gmx.com>
2021-09-23 07:22:31 +0200

    z

--- UI-PrototypeMoviesDBv0.5WPF/Worker.cs
+++ UI-PrototypeMoviesDBv0.5WPF/Worker.cs
@@ -27,15 +27,18 @@ namespace UI_PrototypeMoviesDBv0._5WPF
             for (int i = 0; i < number; )
             {
                 #region Actually Working Area
-                view.UpdateTextBox(i.ToString());
-                view.ScrollToEnd();
+
+                var text = "00h:00m:00s (remaining: 00h:00m:00s)";
                 if (i > 0)
                 {
                     var timeLeft = TimeSpan.FromMilliseconds((number - i) * ((int)timer.Elapsed.TotalMilliseconds / i));
-                    var text = $"{timer.Elapsed.Hours:D2}h:{timer.Elapsed.Minutes:D2}m:{timer.Elapsed.Seconds:D2}s (remaining: {timeLeft.Hours:D2}h:{timeLeft.Minutes:D2}m:{timeLeft.Seconds:D2}s)";
-                    view.UpdateStatusTextTime(text);
-                    view.UpdateTextBox($"{i}\t" + text);
+                    text = $"{timer.Elapsed.Hours:D2}h:{timer.Elapsed.Minutes:D2}m:{timer.Elapsed.Seconds:D2}s (remaining: {timeLeft.Hours:D2}h:{timeLeft.Minutes:D2}m:{timeLeft.Seconds:D2}s)";
                 }
+
+                view.UpdateStatusTextTime(text);
+                view.UpdateTextBox($"{i}\t" + text);
+                view.ScrollToEnd();
+
                 view.UpdateStatusTextTask($"{string.Format("{0:0,0}", (i + 1))} of {string.Format("{0:0,0}", (number))}");
                 view.UpdateStatusProgressBar();
                 view.UpdateStatusTextPercentage($"{((i + 1) / (double)number * 100):F2}%");
